3bcec7f72064d714235710194998ac43e9de315a
CD MacBook <corydavis1989@gmail.com>
2021-12-03 11:40:32 -0600

    new stylings for container

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+let questions = [
+    {
+      question : "What does HTML stand for?",
+      choiceA : "A. Hyper Text Markup Language",
+      choiceB : "B. HyperLinks and Text Markup Language",
+      choiceC : "C. Home Tool Markup Language",
+      choiceD : "D. Home and Tech Media Language",
+      correct : "A"
+    },
+    {
+      question : "What tag is used to define a hyperlink, or link to another page?",
+      choiceA : "A. strong",
+      choiceB : "B. a",
+      choiceC : "C. blockquote",
+      choiceD : "D. em",
+      correct : "B"
+    }];
+
+
+    // create a function to render through questions
+    // for loop maybe?
+
+//     for(questions = 1; questions <= 2; questions++) {
+//         if(questions = 1) {
+//         console.log(questions[1]);
+//     }
+// }
