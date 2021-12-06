460e8f4663369342980da9d099dd8ec8b57d3495
cdtest <corydavis1989@gmail.com>
2021-12-06 13:06:02 -0600

    startbtn/text placement

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,11 +18,87 @@ let questions = [
     }];
 
 
+
+
+
+
+
+    
+var i = 0
+let addQuestion = document.querySelector (".UTBootCamp");
+let addAnswerChoice = document.querySelector (".answerContent")
+let answerButton = document.querySelector (".g-luck");
+
+
+// define Start Button with a variable
+let startbtn = document.querySelector(".start");
+// added an EventListener so a click can occur
+startbtn.addEventListener("click", startQuiz);
+
+
+// added a function to show the questions
+
+function startQuiz () {
+
+  // Clears the container
+  let questionContainer = document.querySelector(".instructions");
+  questionContainer.style.display = "none";
+  let goodLuckContainer = document.querySelector(".g-luck");
+  goodLuckContainer.style.display = "none";
+
+  // Adds Questions
+  addQuestion.innerHTML = questions[i].question;
+
+  // Adds Answer Buttons
+
+  // Adds Answer Button Content
+  addAnswerChoice.innerHTML = questions[i].choiceA;
+  // addAnswerChoice.innerHTML = questions[i].choiceB;
+  // addAnswerChoice.innerHTML = questions[i].choiceC;
+  // addAnswerChoice.innerHTML = questions[i].choiceD;
+
+
+  
+  // let findDiv = document.querySelector (".questionContent");
+  // let questionTitle = document.createElement ("p");
+  // findDiv.append(questionTitle);
+
+  // let answerBtn = document.createElement ("button");
+  // findDiv.appendChild(answerChoice);
+  // answerChoice = questions[i].choiceA;
+
+
+
+  function checkAnswer () {
+
+  }
+
+  function scoreQuestion () {
+
+  }
+
+  function incrementQuestion () {
+
+  }
+
+
+
+  //create buttons for the choice options
+  //the buttons will have their own event listener on them
+  //The above event listner will need to:
+  //  1. check to see if the user is right
+  //  2. affect the score in some way (affect time left as well if they're wrong)
+  //  3. increment i (i++)
+}
+
+//you'll want a separate function for the 3 things above ^^
+//that way when you run an eventListener on the answer buttons you can feed it that function 
+//answerBtn.addEventListener("click", checkAnswer)
+
     // create a function to render through questions
     // for loop maybe?
+    //for(var i = 0; i < length of the questions; i++) {}
+    //i++ - we're going to increment i when a user clicks on an answer button
+    //when they click on the button i will move up by 1 and our index number on the array will move up with it
+    
 
-//     for(questions = 1; questions <= 2; questions++) {
-//         if(questions = 1) {
-//         console.log(questions[1]);
-//     }
-// }
